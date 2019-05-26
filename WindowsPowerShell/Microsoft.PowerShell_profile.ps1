39b9be0bfa0845e812bfe657ef3bf034fd99b761
nidzo <16163816+nidzov@users.noreply.github.com>
2019-05-26 15:52:10 +0200

    Update Microsoft.PowerShell_profile.ps1
    - Edited the way the prompt is rewritten
    - Added oh-my-posh module
    - Added oh-my-posh theme

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,37 @@
 Set-PSReadlineOption -BellStyle None
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+$scripts_folder = "$HOME\Documents\WindowsPowerShell\Scripts\"
+Import-Module "$scripts_folder\GitLogLive.ps1", "$scripts_folder\Get-ComputerInfo.ps1"
 Import-Module posh-git
 
-$profile_folder = "$env:USERPROFILE\Documents\WindowsPowerShell"
-Import-Module $("$profile_folder\Functions.ps1")
+$is_conemu = $env:ConEmuANSI
 
-$LAMBDA = ([char]955)
-
-function global:prompt {
-    $realLASTEXITCODE = $LASTEXITCODE
-    $path = Convert-Path .
-    
-    if ($path -eq "") {
-        "$pwd $LAMBDA"
-    }
-    else {
-        Write-Host "$(Split-Path $pwd -Leaf)" -NoNewline 
-    }
-    Write-VcsStatus
-    Write-Host " $LAMBDA" -NoNewline
-
-    $global:LASTEXITCODE = $realLASTEXITCODE
-    $host.UI.RawUI.WindowTitle = $pwd
-    return " "
+if ($is_conemu -eq "ON") {
+    Import-Module oh-my-posh
+    Set-Theme Robbyrussell
 }
+else {
+    # $LAMBDA = ([char]955)
+    function global:prompt {
+        $realLASTEXITCODE = $LASTEXITCODE
+        Write-Host "$(Split-Path $pwd -Leaf) >" -NoNewline
+        Write-VcsStatus
+        $global:LASTEXITCODE = $realLASTEXITCODE
+        return " "
+    }
+}
+$host.UI.RawUI.WindowTitle = $pwd
 
 Pop-Location
-
 Set-Alias -Name "ll" -Value "Get-ChildItem"
+function Show-Wlan { Param([string]$a)netsh.exe wlan show profile name=$a key=clear }
+function dotfiles { code "$HOME\dev\windows-dotfiles" }
+function admin { powershell -NoLogo -new_console:a }
+
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
